a129496a181ea6df0f711cb1e00a0729cba4f0d7
Nigel Harsch <nigelharsch@gmail.com>
2014-07-28 17:33:23 -0400

    updates

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,8 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns(' ',
     # Examples:
-    # url(r'^blog/', include('blog.urls')),    
+    # url(r'^blog/', include('blog.urls')),  
+    url(r'^$', include('polls.urls')),  
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls')),
-    url(r'admin/', include(admin.site.urls))
+    url(r'^polls/', include('polls.urls', namespace="polls")),
+    # url(r'admin/', include(admin.site.urls))
 )
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.http import Http404
+
+# from django.template import RequestContext, loader
+
+from polls.models import Poll
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, world. You're at the poll index.")
+	# return HttpResponse("Hello, world. You're at the poll index.")
+	#this is the simplest view possible in Django. 
+	#To call view, we need to map it to a URL = and for this we need a URLconf
+
+	# latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+	# template = loader.get_template('polls/index.html')
+	# context = RequestContext(request, {
+	# 	'latest_poll_list': latest_poll_list,
+	# })
+	# # output = ', '.join([p.question for p in latest_poll_list])
+	# return HttpResponse(template.render(context))
+
+	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+	context = {'latest_poll_list': latest_poll_list}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-	return HttpResponse("You're looking at poll %s." % poll_id)
+	# try:
+	# 	poll = Poll.objects.get(pk=poll_id)
+	# except Poll.DoesNotExist:
+	# 	raise Http404
+	# return render(request, 'polls/detail.html', {'poll': poll})
+
+	poll = get_object_or_404(Poll, pk=poll_id)
+	return render(request, "polls/detail.html", {'poll':poll})
 
 def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s. " % poll_id)
 
 def vote(request, poll_id):
 	return HttpREsponse("You're voting on poll %s." % poll_id)
+
 

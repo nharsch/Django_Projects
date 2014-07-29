8983cd30cc86d965a4c0bd596501926dd85090f9
nharsch <nigelharsch@gmail.com>
2014-07-28 20:41:59 -0400

    rango homework

--- Tango_with_Django/tango_with_django_project/tango_with_django_project/urls.py
+++ Tango_with_Django/tango_with_django_project/tango_with_django_project/urls.py
@@ -1,12 +1,22 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from rango import views #not sure if this is the best way
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	#this is a tuple
+
+	#it must be named urlpatterns
+
     # Examples:
     # url(r'^$', 'tango_with_django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+	#this maps empty strings to index
+
+    # url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^rango/', include('rango.urls'))
 )

8df9ca5fcd6b5da22dae434df75b6d93f212f636
nharsch <nigelharsch@gmail.com>
2014-08-14 20:20:17 -0400

    vbox changes

--- OVS_Packager_Batch_Import/CSVImport/CSVImporter/views.py
+++ OVS_Packager_Batch_Import/CSVImport/CSVImporter/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View 
 
-# Create your views here.
+class BatchJobView(View):
+	def get(self, request):
+	# view logic
+		return HttpResponse('result')
+
+

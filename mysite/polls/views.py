dd383c7dad518b5a3de96c2b73d5bf553987d2ca
nharsch <nigelharsch@gmail.com>
2014-08-15 12:13:57 -0400

    mac changes

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.http import Http404
+from polls.models import Choice, Poll
 
 # from django.template import RequestContext, loader
 
@@ -38,6 +39,19 @@ def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s. " % poll_id)
 
 def vote(request, poll_id):
+	p = get_object_or_404(Poll, pk=poll_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# REdisplay the poll voting form.
+		return render(request, 'polls/detail.html', {
+			'poll': p, 
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully dealing
 	return HttpREsponse("You're voting on poll %s." % poll_id)
 
 

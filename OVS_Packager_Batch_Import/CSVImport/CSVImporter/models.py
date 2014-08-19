92738bed87208697975a416b92867103e2875db9
nharsch <nigelharsch@gmail.com>
2014-08-19 20:02:18 +0000

    git ingores

--- OVS_Packager_Batch_Import/CSVImport/CSVImporter/models.py
+++ OVS_Packager_Batch_Import/CSVImport/CSVImporter/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class BatchJob(models.Model):
+class PackJob(models.Model):
 	prodid = models.IntegerField(max_length= 8)
 	title = models.CharField(max_length=144)
 	asset_type = models.IntegerField(max_length= 1) #consider making this a choice field
-	priority = models.IntegerField(max_length= 2)
-	## possibly add "job_types = (package, delete...)"
+	priority = models.IntegerField(max_length= 2) 
+  ## possibly add "job_types = (package, delete...)"
+ 
+job1 = PackJob()
+
+print job1
 

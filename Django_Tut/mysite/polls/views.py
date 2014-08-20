54a85b896d4efba6b88471086509706059bf07cb
nharsch <nigelharsch@gmail.com>
2014-08-19 23:51:03 -0400

    completed seciton 4

--- Django_Tut/mysite/polls/views.py
+++ Django_Tut/mysite/polls/views.py
@@ -1,42 +1,60 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.http import Http404
-from polls.models import Choice, Poll
+from django.shortcuts import render, get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 
 # from django.template import RequestContext, loader
 
-from polls.models import Poll
+from polls.models import Choice, Poll
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_poll_list'
+
+	def get_queryset(self):
+		"""REturn the last five published polls."""
+		return Poll.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/results.html'
 
 # Create your views here.
-def index(request):
-	# return HttpResponse("Hello, world. You're at the poll index.")
-	#this is the simplest view possible in Django. 
-	#To call view, we need to map it to a URL = and for this we need a URLconf
+# def index(request):
+# 	# return HttpResponse("Hello, world. You're at the poll index.")
+# 	#this is the simplest view possible in Django. 
+# 	#To call view, we need to map it to a URL = and for this we need a URLconf
 
-	# latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	# template = loader.get_template('polls/index.html')
-	# context = RequestContext(request, {
-	# 	'latest_poll_list': latest_poll_list,
-	# })
-	# # output = ', '.join([p.question for p in latest_poll_list])
-	# return HttpResponse(template.render(context))
+# 	# latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+# 	# template = loader.get_template('polls/index.html')
+# 	# context = RequestContext(request, {
+# 	# 	'latest_poll_list': latest_poll_list,
+# 	# })
+# 	# # output = ', '.join([p.question for p in latest_poll_list])
+# 	# return HttpResponse(template.render(context))
 
-	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-	context = {'latest_poll_list': latest_poll_list}
-	return render(request, 'polls/index.html', context)
+# 	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+# 	context = {'latest_poll_list': latest_poll_list}
+# 	return render(request, 'polls/index.html', context)
 
-def detail(request, poll_id):
-	# try:
-	# 	poll = Poll.objects.get(pk=poll_id)
-	# except Poll.DoesNotExist:
-	# 	raise Http404
-	# return render(request, 'polls/detail.html', {'poll': poll})
+# def detail(request, poll_id):
+# 	# try:
+# 	# 	poll = Poll.objects.get(pk=poll_id)
+# 	# except Poll.DoesNotExist:
+# 	# 	raise Http404
+# 	# return render(request, 'polls/detail.html', {'poll': poll})
 
-	poll = get_object_or_404(Poll, pk=poll_id)
-	return render(request, "polls/detail.html", {'poll':poll})
+# 	poll = get_object_or_404(Poll, pk=poll_id)
+# 	return render(request, "polls/detail.html", {'poll':poll})
 
-def results(request, poll_id):
-	return HttpResponse("You're looking at the results of poll %s. " % poll_id)
+# def results(request, poll_id):
+# 	poll = get_object_or_404(Poll, pk=poll_id)
+# 	#return HttpResponse("You're looking at the results of poll %s. " % poll_id)
+# 	return render(request, 'polls/results.html', {'poll': poll})
 
 def vote(request, poll_id):
 	p = get_object_or_404(Poll, pk=poll_id)
@@ -52,6 +70,8 @@ def vote(request, poll_id):
 		selected_choice.votes += 1
 		selected_choice.save()
 		# Always return an HttpResponseRedirect after successfully dealing
-	return HttpREsponse("You're voting on poll %s." % poll_id)
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+	return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
 

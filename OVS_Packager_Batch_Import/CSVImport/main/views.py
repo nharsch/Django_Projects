1485bf45f4cba5764a5e497e94d7986ce2423c21
nharsch <nigelharsch@gmail.com>
2014-08-20 21:46:57 +0000

    nitrous adds

--- OVS_Packager_Batch_Import/CSVImport/main/views.py
+++ OVS_Packager_Batch_Import/CSVImport/main/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
-from scripts import Batch_Submit_Jobs_Dev 
+from .Batch_Submit_Jobs_Dev import API_submit
  
 from forms import UploadFileForm
 from models import UploadFile
 # Create your views here.
 
- 
  
 def home(request):
     if request.method == 'POST':
@@ -17,11 +16,20 @@ def home(request):
         if form.is_valid():
             new_file = UploadFile(file = request.FILES['file'])
             new_file.save()
-            Batch_Submit_Jobs_Dev(file)
+          #  jobs_list = API_submit(new_file)
+            ##this will be a list of dicts of jobs
  
-            return HttpResponseRedirect(reverse('main:home'))
+            #return HttpResponseRedirect(reverse('main:home'))
+            return HttpResponse("test")
     else:
         form = UploadFileForm()
  
     data = {'form': form}
     return render_to_response('main/index.html', data, context_instance=RequestContext(request))
+    #return HttpResponse("test")
+
+def response(request):
+    pass
+    #show response table
+    #link to OVS packager job list
+    

36409460593f605253af9dc487ae68a7a519bfa6
nharsch <nigelharsch@gmail.com>
2014-08-22 15:29:03 +0000

    changes

--- OVS_Packager_Batch_Import/CSVImport/main/models.py
+++ OVS_Packager_Batch_Import/CSVImport/main/models.py
@@ -7,10 +7,10 @@ class UploadFile(models.Model):
     file = models.FileField(upload_to='files/%Y/%m/%d')
 
 class PackJob(models.Model):
-  	prodid = models.IntegerField(unique=False)
+  	prodid = models.CharField(max_length=12)
 	title = models.CharField(max_length=300)
-	asset_type = models.IntegerField() #consider making this a choice field
-	priority = models.IntegerField() 
+	asset_type = models.CharField(max_length=10) #consider making this a choice field
+	priority = models.IntegerField(unique=False) 
         def __unicode__(self):
             return self.title
     ##column1=prodid, column2=title, column3=asset_type, column4=priority
